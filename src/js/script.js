71d6d55fe6a388ee0ff9dc942f711589f02f1a63
Mohammad Nanda Rizky Dhiya'ulhaq <mohnandarizky@gmail.com>
2021-02-18 16:46:22 +0700

    all done

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,8 @@
 const navPos = window.pageYOffset;
 const nav = document.querySelector("nav");
+const mobileNav = document.getElementById("mobile-nav");
+const toggleNav = document.getElementById("menu-toggle");
+const backdrop = document.getElementById("backdrop-body");
 
 if (navPos > 1) {
   nav.classList.add("scroll-nav");
@@ -9,11 +12,23 @@ if (navPos > 1) {
 
 document.addEventListener("scroll", function () {
   const scrollPos = window.pageYOffset;
-  console.log(scrollPos)
+  console.log(scrollPos);
 
   if (scrollPos > 50) {
     nav.classList.add("scroll-nav");
   } else {
     nav.classList.remove("scroll-nav");
+  }
+});
+
+toggleNav.addEventListener("click", function () {
+  mobileNav.classList.toggle("show");
+  backdrop.classList.toggle("show");
+  document.body.classList.toggle("freeze");
+
+  if (mobileNav.classList.contains("show")) {
+    toggleNav.setAttribute("src", "/src/assets/close-menu.svg");
+  } else {
+    toggleNav.setAttribute("src", "/src/assets/hamburger-menu.svg");
   }
 });
