83bafd5ca421cddc8a6bc849bea941af16346218
Mohammad Nanda Rizky Dhiya'ulhaq <mohnandarizky@gmail.com>
2021-02-18 15:17:36 +0700

    all done

--- src/js/script.js
+++ src/js/script.js
@@ -1,58 +1,19 @@
-// AOS
-  AOS.init();
+const navPos = window.pageYOffset;
+const nav = document.querySelector("nav");
 
-const mobileNav = document.getElementById("mobile-nav");
-const blur = document.getElementById("blur");
-
-document.addEventListener("scroll" ,function(){
-  const scrollpos = window.pageYOffset;
-  const nav = document.querySelector("nav");
-  
-  if(scrollpos > 150){
-    nav.classList.add("navShow");
-    mobileNav.classList.add("followNav");
-    blur.classList.add("followNav");
-  } else{
-    nav.classList.remove("navShow");
-    mobileNav.classList.remove("followNav");
-    blur.classList.remove("followNav");
-  }
-});
-
-const scrollIndicator = document.querySelector(".scroll-indicator");
-scrollIndicator.addEventListener("click", function(){
-  location.href = "#hp1"
-});
-
-
-
-// Mobile nav logic
-const menuToggle = document.querySelector(".toggle");
-
-menuToggle.addEventListener("click", function(){
-  navState();
-  mobileNav.classList.toggle("show");
-  blur.classList.toggle("show");
-  document.body.classList.toggle("freeze");
-});
-
-const mobileLink = document.querySelectorAll("#mobile-nav a");
-mobileLink.forEach(function(link){
-  link.addEventListener("click", function(){
-    navState();
-    mobileNav.classList.toggle("show");
-    blur.classList.toggle("show");
-    document.body.classList.toggle("freeze");
-  });
-});
-
-
-
-// state for icon nav
-const navState = () => {
-  if(mobileNav.classList.contains("show")){
-    menuToggle.setAttribute("src", "/src/assets/hamburger-menu.svg")
-  }else{
-    menuToggle.setAttribute("src", "/src/assets/close-menu.svg")
-  }
+if (navPos > 1) {
+  nav.classList.add("scroll-nav");
+} else {
+  nav.classList.remove("scroll-nav");
 }
+
+document.addEventListener("scroll", function () {
+  const scrollPos = window.pageYOffset;
+  console.log(scrollPos)
+
+  if (scrollPos > 50) {
+    nav.classList.add("scroll-nav");
+  } else {
+    nav.classList.remove("scroll-nav");
+  }
+});
